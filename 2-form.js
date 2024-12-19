655afe9ecf7e2d2159fce176a69c58e629eddb73
evitan22 <kokravets@gmail.com>
2024-12-20 01:22:23 +0200

    made task2

--- 2-form.js
+++ 2-form.js
@@ -1,12 +1,35 @@
-formData = {
-    email: "",
-    message: ""
+const formData = {
+    email: '',
+    message: ''
 }
 
-localStorage.setItem('feedback-form-state', 'HEllo');
-
+const form = document.querySelector('form');
+const input = document.getElementsByTagName('input')[0];
+const textArea = document.getElementsByTagName('textarea')[0];
 const raw = localStorage.getItem('feedback-form-state');
-const person = JSON.parse(raw);
-console.log(person)
-person.email = 'erw@isdgjs'
-console.log(person)
+
+if(raw) {
+    const obj = JSON.parse(raw);
+    input.value = obj.email;
+    textArea.value = obj.message;
+    formData.email = obj.email;
+    formData.message = obj.message;
+}
+
+form.addEventListener('input', e => {
+    formData[e.target.name] += e.data;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+})
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    if (!input.value || !textArea.value) {
+        alert('Fill please all fields');
+    }
+    else {
+        console.log({...formData});
+        formData.email = '';
+        formData.message = '';
+        form.reset();
+    }
+})
